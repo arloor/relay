23380ba2121fa044532f49a7d2eb40e83e0dcc52
arloor <admin@arloor.com>
2021-09-21 15:24:35 +0800

    relay

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use tokio::fs::File;
-use tokio::io::{self, AsyncReadExt};
 use tokio::net::{TcpListener, TcpStream};
 use std::env;
-use std::sync::Arc;
+use tokio::io;
+use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -12,17 +11,32 @@ async fn main() -> io::Result<()> {
     let target = Arc::new(String::from(&args[2]));
     let listener = TcpListener::bind(from).await?;
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let (socket, addr) = listener.accept().await?;
         let target = Arc::clone(&target);
         tokio::spawn(async move {
             let remote = TcpStream::connect(&*target).await.unwrap();
-            let (mut remoteReader, mut remoteWriter) = io::split(remote);
+            let (mut remote_reader, mut remote_writer) = io::split(remote);
             let (mut reader, mut writer) = io::split(socket);
-            tokio::spawn(async move {
-                io::copy(&mut remoteReader, &mut writer).await;
+            let remote_handler = tokio::spawn(async move {
+                let n = io::copy(&mut remote_reader, &mut writer).await.unwrap_or_else(|e| {
+                    println!("remote -> local {}", e);
+                    0
+                });
+                println!("{} read remote {} bytes", addr, n);
             });
-            io::copy(&mut reader, &mut remoteWriter).await;
+
+            let local_handler = tokio::spawn(async move {
+                let n = io::copy(&mut reader, &mut remote_writer).await.unwrap_or_else(|e| {
+                    println!("local -> remote {}", e);
+                    0
+                });
+                println!("{} read local {} bytes", addr, n);
+            });
+
+            remote_handler.await;
+            local_handler.abort();
+            // tokio::join!(local_handler,remote_handler);
+
         });
     }
-    Ok(())
 }
